31929f02577f73b2494b7032a8a805e4d54a6b28
Lucas <lucasmilessi98@gmail.com>
2022-10-17 20:45:47 -0300

    Arreglos de export excel

--- src/components/tabla/ListarPasturas.jsx
+++ src/components/tabla/ListarPasturas.jsx
@@ -4,6 +4,7 @@ import "../../style/components/tabla/listarPasturas.css"
 import { ActualizarPastura } from "../formulario/ActualizarPastura";
 import  ReactHtmlTableToExcel  from "react-html-table-to-excel";
 
+
 export const ListarPasturas = ({ listPasturas }) => {
 
     const [clickEdit, setClickEdit] = useState(false);
@@ -26,9 +27,10 @@ export const ListarPasturas = ({ listPasturas }) => {
         }).then(res => console.log(res))
     }
 
+
   return (
     <>
-        <div align="center">
+         <div align="center">
             <ReactHtmlTableToExcel 
                 id="botonExportExcel"
                 className="btn btn-primary"
@@ -38,9 +40,74 @@ export const ListarPasturas = ({ listPasturas }) => {
                 buttonText="Exportar a Excel"
             />
         </div>
+        <div>
+        <table hidden className="table" id="tablaExcel">
+                    <thead className="table-danger">
+                        <tr>
+                            <th>Familia</th>
+                            <th>Especie</th>
+                            <th>Tipo Vegetativo</th>
+                            <th>Rizoma Engrozado</th>
+                            <th>Macollo 1</th>
+                            <th>Macollo 2</th>
+                            <th>Consistecia de la ligula</th>
+                            <th>Forma de la ligula</th>
+                            <th>Tamaño</th>
+                            <th>Otra caracteristica ligula</th>
+                            <th>Color de la ligula</th>
+                            <th>Forma de la lamina</th>
+                            <th>Canaliculada</th>
+                            <th>Tipo de lamina</th>
+                            <th>Apice</th>
+                            <th>Nervadura central marcada</th>
+                            <th>Observaciones</th>
+                            <th>Pelos</th>
+                            <th>Ubicación de pelos</th>
+                            <th>Observación</th>
+                            <th>Observaciones generales</th>
+                            <th>Ciclo de Vida</th>
+                            <th>Ciclo Productivo</th>
+                            <th>Tipo Productivo</th>
+                            <th>Tipo de Campo</th>
+                        </tr> 
+                    </thead>
+                    <tbody>
+                        { listPasturas.map((list) => 
+                            <tr key={list._id}>
+                                <td>{list.familia}</td>
+                                <td>{list.especie}</td>
+                                <td>{list.tipo_vegetativo}</td>
+                                <td>{list.rizoma_engrozado}</td>
+                                <td>{list.macollo1}</td>
+                                <td>{list.macollo2}</td>
+                                <td>{list.consistecia_de_la_ligula}</td>
+                                <td>{list.forma_de_la_ligula}</td>
+                                <td>{list.tamanio}</td>
+                                <td>{list.otra_caracteristica_ligula}</td>
+                                <td>{list.color_de_la_ligula}</td>
+                                <td>{list.forma_de_la_lamina}</td>
+                                <td>{list.canaliculada}</td>
+                                <td>{list.tipo_de_lamina}</td>
+                                <td>{list.apice}</td>
+                                <td>{list.nervadura_central_marcada}</td>
+                                <td>{list.observaciones}</td>
+                                <td>{list.pelos}</td>
+                                <td>{list.ubicación_de_pelos}</td>
+                                <td>{list.observacion}</td>
+                                <td>{list.observaciones_generales}</td>
+                                <td>{list.ciclo_de_vida}</td>
+                                <td>{list.ciclo_productivo}</td>
+                                <td>{list.tipo_productivo}</td>
+                                <td>{list.tipo_de_campo}</td>
+                            </tr>
+                        )}
+                    </tbody>
+                </table>
+        </div>
+
         <div className="view">
             <div className="wrapper">
-                <table className="table" id="tablaExcel">
+                <table className="table">
                     <thead className="table-danger">
                         <tr>
                             <th>Familia</th>
